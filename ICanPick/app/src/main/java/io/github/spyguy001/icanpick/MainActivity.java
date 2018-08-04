4ce98c7482f9ac25fa4d87e66d9577eedc21c7b3
Spyguy001 <amritprabh98@hotmail.com>
2018-08-04 01:31:58 -0400

    Sample code getting the location of a place using Places API

--- ICanPick/app/src/main/java/io/github/spyguy001/icanpick/MainActivity.java
+++ ICanPick/app/src/main/java/io/github/spyguy001/icanpick/MainActivity.java
@@ -1,13 +1,53 @@
 package io.github.spyguy001.icanpick;
 
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    protected GeoDataClient mGeoDataClient;
+    protected PlaceDetectionClient mPlaceDetectionClient;
+    final String latitude = "40.7463956";
+    final String longtitude = "-73.9852992";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView tv1 = (TextView)findViewById(R.id.text);
+        //tv1.setText("Hey");
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+        String placeId = "ChIJv1rQpeoVK4gRd4bDfGYxLTU";
+        mGeoDataClient.getPlaceById(placeId).addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
+                if (task.isSuccessful()) {
+                    PlaceBufferResponse places = task.getResult();
+                    Place myPlace = places.get(0);
+                    Log.i(TAG, "Place found: " + myPlace.getName());
+                    TextView tv1 = (TextView)findViewById(R.id.text);
+                    tv1.setText(myPlace.getName());
+                    places.release();
+                } else {
+                    Log.e(TAG, "Place not found.");
+                }
+            }
+        });
     }
 }
